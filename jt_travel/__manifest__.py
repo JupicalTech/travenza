bbc4ddeedf40554036b53dcb14fc652a916b7124
dhara-jupical <dhara.jupical@gmail.com>
2026-07-30 17:39:11 +0530

    [IMP]Activated all the currencies in previous push so now hiding the accounting menu again

--- jt_travel/__manifest__.py
+++ jt_travel/__manifest__.py
@@ -24,7 +24,7 @@
     'name': 'Travenza Holidays',
     'summary': 'Comprehensive Travel CRM and Operations Management for Travenza Holidays',
     'description': 'A comprehensive ERP solution for managing travel inquiries, specialized holiday quotations, automated project creation for bookings, and document-linked billing processes.',
-    'version': '19.0.1.0.5',
+    'version': '19.0.1.0.6',
     'author': 'Jupical Technologies Pvt. Ltd.',
     'maintainer': 'Jupical Technologies Pvt. Ltd.',
     'website': 'https://www.jupical.io',
